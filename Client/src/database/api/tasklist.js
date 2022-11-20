7b9c2e669dac02a52aa707a0a8df01c7c311e37d
Krishna Le Moing <krishna.lemoing@yahoo.com>
2022-11-20 01:53:17 +0100

    UserFetchApi

--- Client/src/database/api/tasklist.js
+++ Client/src/database/api/tasklist.js
@@ -6,8 +6,8 @@ import { db } from "../config";
 // Query to get users task list
 export const queryTasks = async (user) => {
     try {
-        // const q = query(collection(db, "taskList"), where("userID", "==", user.userId));
-        const q = query(collection(db, "taskList"));
+         const q = query(collection(db, "taskList"), where("userId", "==", user.userId));
+        //const q = query(collection(db, "taskList"));
         const querySnapshot = await getDocs(q);
 
         const docs = querySnapshot.forEach((doc) => {
@@ -23,21 +23,21 @@ export const queryTasks = async (user) => {
 
 // Add task to tasklist
 
-/* export const newTasktoTasklist = async (title, details, deadline, author) => {
+/*  export const newTasktoTasklist = async (title, details, deadline, user) => {
 await setDoc(doc(db, "tasklist"),{
     title : title,
-    userId : author,
+    userId : user.userId,
     deadline : deadline,
     details : details,
     date : Timestamp.now(),
     status : 'not completed'
 })
-} */
+}  */
 
 // delete task
 
-/* export const deleteTask = async (title, dispatch) => {
-    await deleteDoc(doc(db, "tasklist", `${title}`));
+/*  export const deleteTask = async (title) => {
+    await deleteDoc(doc(db, "taskList", where("title", "==", title)));
     //dispatch(your_tasks());
-} */
+}  */
 
